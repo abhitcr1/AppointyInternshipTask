36172adc4a7fc0602ba25aff59288f5ed15d963c
Abhit Anand <abhitcr1@gmail.com>
2021-10-09 17:29:46 +0530

    added mongodb coonection from website's doc

--- server.go
+++ server.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 type User struct {
 	ID    primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
@@ -27,4 +31,27 @@ func main() {
 	router.POST("/articles", createUser)
 	// set our port address as 8081
 	log.Fatal(http.ListenAndServe(":8081", router))
+}
+// ConnecttoDB : function to connect to mongoDB locally
+func ConnecttoDB() *mongo.Collection {
+
+	// Set client options
+	//change the URI according to your database
+	clientOptions := options.Client().ApplyURI("mongodb+srv://abhitcr1:Abhit1010@cluster0.yg0tb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+	
+	// Connect to MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	//Error Handling
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	//DB collection address which we are going to use
+	//available to functions of all scope
+	collection := client.Database("Appointy").Collection("InstaPosts")
+
+	return collection
 }
