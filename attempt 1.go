7d23034be8da5c33427a519ee75c70a8fbefb055
Abhit Anand <abhitcr1@gmail.com>
2021-10-09 23:50:42 +0530

    final commit

--- attempt 1.go
+++ attempt 1.go
@@ -6,8 +6,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"path"
+	"crypto/md5"
 
-	"github.com/julienschmidt/httprouter"
+	
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -57,6 +59,25 @@ func ConnecttoDB() *mongo.Collection {
 
 	return collection
 }
+func findUser(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+  
+	  //Extracting the Path of URL
+	  url := r.URL.RequestURI()
+	  key := path.Base(url)
+  
+	var result bson.M
+  
+	//Finding the user
+	err := collection.FindOne(context.TODO() , bson.M{"userId" : key}).Decode(&result)
+	if err != nil {
+	  log.Fatal(err)
+	}
+  
+	//Sending the data Back
+	json.NewEncoder(w).Encode(result)
+  }
+  
 //Function to get all user in DataBase
 func getUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
@@ -100,23 +121,47 @@ func getUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	json.NewEncoder(w).Encode(user)
 
 }
+
 //Function to create a new user in Database
-func createuser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func createUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	var user user
+  //Checking if the method is the post method or not
+  if r.Method == "POST"{
+    //Parsing the Values
+    r.ParseForm()
 
-	//Decoding the Data from JSON format to user variable
-	_ = json.NewDecoder(r.Body).Decode(&user)
-	//inserts the data from decoded var to MongoDB in BSON format
-	result, err := collection.InsertOne(context.TODO(), user)
-	//Error Handling
-	if err != nil {
-		log.Fatal(err)
-	}
+    //Hashing the Passowrd
+    pass := md5.Sum([]byte(r.Form.Get("email")));
 
-	json.NewEncoder(w).Encode(result)
-}
+    //Pushing the data into DB
+	  result, err := collection.InsertOne(context.TODO(),bson.D{
+    {Key: "name", Value: r.Form.Get("name")},
+    {Key: "passowrd", Value: string(pass[:])},
+    {Key : "email" , Value : r.Form.Get("email")},
+    {Key : "userId" , Value : r.Form.Get("id")},
+})
+//Function to create a POST
+func createPost(w http.ResponseWriter, r *http.Request){
+    w.Header().Set("Content-Type", "application/json")
+
+    //Checking the Method is POST
+    if r.Method == "POST"{
+      //Parsing the Form
+      r.ParseForm()
+
+      //Adding the timestamp
+      dt := time.Now();
+      dt.Format("01-02-2006 15:04:05")
+
+      //Pushing the Data into DB
+  	  result, err := postCollection.InsertOne(context.TODO(),bson.D{
+      {Key: "id", Value: r.Form.Get("pid")},
+      {Key: "caption", Value: r.Form.Get("caption")},
+      {Key : "url" , Value : r.Form.Get("url")},
+      {Key : "time" , Value : dt.String()},
+      {Key : "userId" , Value : r.Form.Get("userId")},
+  })
 //Function to search User by ID
 func getUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	w.Header().Set("Content-Type", "application/json")
@@ -138,10 +183,32 @@ func getUser(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 	json.NewEncoder(w).Encode(User)
 }
-
+func userPost(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+  
+	//Extracting the UserId
+	url := r.URL.RequestURI()
+	key := path.Base(url)
+  
+	//Finding the posts made by that user
+	filterCursor, err := postCollection.Find(context.TODO(), bson.M{"userId": key})
+	if err != nil {
+		log.Fatal(err)
+	}
+  
+	//Appending it to the array and sending it back
+	var postFilter []bson.M
+	if err = filterCursor.All(context.TODO(), &postFilter); err != nil {
+		log.Fatal(err)
+	}
+  
+	json.NewEncoder(w).Encode(postFilter)
+  
+  }
+  
 fmt.Fprintf(w, `Hello world`)
 func searchUserUsingID(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	var article Article
+	var InstaPosts InstaPosts
 
 	//recovers the argument of search query present in URL after "q"
 	title := string(r.URL.Query().Get("q"))
@@ -150,10 +217,10 @@ func searchUserUsingID(w http.ResponseWriter, r *http.Request, ps httprouter.Par
 	filter := bson.M{"title": title}
 
 	//Searching in DB with given title as keyword
-	err := collection.FindOne(context.TODO(), filter).Decode(&article)
+	err := collection.FindOne(context.TODO(), filter).Decode(&InstaPosts)
 	//Error Handling
 	if err != nil {
 		log.Fatal(err)
 	}
-	json.NewEncoder(w).Encode(article)
+	json.NewEncoder(w).Encode(InstaPosts)
 }
